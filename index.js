b7357d30982f39c73b1c84ed5ba931185155ebf5
camryn williams <williams.camryn.e@gmail.com>
2021-03-18 15:09:52 -0400

    commit tree a049ef3e4b358c8d0722bf0886f7113b05803565

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'also bob';
+
+
+function upperCaseCustomerName(){
+    this.customerName = this.customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    this.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    this.bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'carl';
+}
